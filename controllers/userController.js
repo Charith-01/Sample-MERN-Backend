fc8db58e65100d118598fe3a3874b52569b5eb6c
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-04 17:13:44 +0530

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,23 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
 export function saveUser(req, res){
-    
+
     const hashPassword = bcrypt.hashSync(req.body.password, 10);
-    console.log(hashPassword);
+    
+    const user = new User({
+        email : req.body.email,
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        password : hashPassword
+    })
+
+    user.save().then(()=>{
+        res.json({
+            message: "Usr saved successfully"
+        })
+    }).catch(()=>{
+        res.json({
+            message: "User not saved"
+        })
+    })
 }
