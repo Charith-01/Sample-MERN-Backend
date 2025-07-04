044774e8bee9785cb6425612ac08b0a9910d7331
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-04 16:40:24 +0530

    Update userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,9 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
+
+export function saveUser(req, res){
+
+    const hashPassword = bcrypt.hashSync(req.body.password);
+    console.log(hashPassword);
+}
 
