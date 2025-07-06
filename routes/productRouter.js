0678115d8aef9b5c59ed7ffc62ac24efc5b2c371
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-07 01:39:21 +0530

    Update
    Co-Authored-By: Vihangi Jayasinghe <vihangijayasinghe10@gmail.com>
    Co-Authored-By: Hasan Kavishka <219181606+Hasan-hkh@users.noreply.github.com>
    Co-Authored-By: Ishara Wijerathna <IT23824874@my.sliit.lk>

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getProducts } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", createProduct);
+productRouter.get("/", getProducts);
+productRouter.delete("/:productId", deleteProduct);
 
 export default productRouter;
